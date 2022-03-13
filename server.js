8cf8d5ea551305e1fb47cc7f0eed2b36076bfc46
CommanderSalt <101530221+CommanderSalt@users.noreply.github.com>
2022-03-14 00:02:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const PORT = process.env.PORT || 80
 const returndata = {
-    ["done"]: "not released yet:(",
+    ["done"]: "eddie has small pp",
 }
 
 const writeErr = (res, code, message) => {
