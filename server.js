729474849bb9ab492053a12f30126f1d7eb9ee8e
CommanderSalt <101530221+CommanderSalt@users.noreply.github.com>
2022-03-14 00:04:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const PORT = process.env.PORT || 80
 const returndata = {
-    ["done"]: "eddie has small pp",
+    ["done"]: "tlx when",
 }
 
 const writeErr = (res, code, message) => {
