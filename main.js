c1c2370e1db402a46943271f919d42370197795a
CommanderSalt <101530221+CommanderSalt@users.noreply.github.com>
2023-12-12 22:19:38 +0000

    Changes

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const PORT = process.env.PORT || 80
+const server = http.createServer()
 
+server.on("request", async function(req, res){
+    req.on("error", function(err){
+        console.error(err)
+    })
+    res.statusCode = 200
+    res.end(data)
+})
+
+server.listen(PORT, (err) => {
+    if (err){
+        console.error(`Server listening error: ${err}`)
+        return
+    }
+    console.log(`Server started on port ${PORT}`)
+})
